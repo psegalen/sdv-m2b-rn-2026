3d51837bbf0173c2e39ba9b32969355ff5c047f1
Pierre Segalen <psegalen@outlook.com>
2026-07-09 15:01:18 +0200

    Added OS & Device designations in Profile Screen

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,11 +1,31 @@
-import { Text, View } from "react-native";
+import * as Device from "expo-device";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>Utilisateur principal</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Utilisateur principal</Text>
+      <Text style={styles.text}>
+        OS : {Platform.OS} v{Device.osVersion}
+      </Text>
+      <Text style={styles.text}>
+        Modèle : {Device.manufacturer}{" "}
+        {Platform.select({ ios: Device.modelName, android: Device.designName })}
+      </Text>
     </View>
   );
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 32,
+  },
+  text: {
+    fontSize: 22,
+  },
+});
